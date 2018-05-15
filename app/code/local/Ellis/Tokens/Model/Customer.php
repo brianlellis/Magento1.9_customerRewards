c34202b98d4ab2bd9ad51ac052c833f70344cd4a
Brian Ellis <brian@briansresume.com>
2018-05-15 08:27:19 -0400

    README

---
+++ app/code/local/Ellis/Tokens/Model/Customer.php
@@ -0,0 +1,12 @@
+<?php
+class Ellis_Tokens_Model_Customer extends Mage_Customer_Model_Customer{
+
+    /**
+     * Validate customer attribute values.
+     *
+     * @return bool
+     */
+    public function validate() {
+     echo 'I am here'; die;
+   }
+}
